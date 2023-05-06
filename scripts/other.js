8c6bfeebcb42b1b011f4533a68eeccdff6572bd5
Franco Ojeda <fojeda@slingr.io>
2023-05-06 15:57:50 -0300

    refactor

---
+++ scripts/other.js
@@ -0,0 +1,9 @@
+exports.packageName = function () {
+    return "nested-dep1";
+}
+
+exports.nested1 = function () {
+    var versionResponse = "Version response: " + dependencies.versionTest.versionLib.versionValue();
+    var dependencyResponse = "Dependency response: " + dependencies.nested-dep2.foo.nested2()
+    return "{{From package [nested-dep1] |\n " + dependencyResponse + " |\n " + versionResponse + "}}";
+}
--- scripts/test2.js
+++
@@ -1,7 +0,0 @@
-exports.aPlusB = function (a, b) {
-    return a+b;
-}
-
-exports.otherTest = function () {
-    return "other test 3 ";
-}
