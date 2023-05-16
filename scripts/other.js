b8f7b6510d179cd0053f08f7811fbb89c631510c
Franco Ojeda <fojeda@slingr.io>
2023-05-16 17:48:09 -0300

    change function call

--- scripts/other.js
+++ scripts/other.js
@@ -3,7 +3,7 @@ exports.packageName = function () {
 }
 
 exports.nested1 = function () {
-    var versionResponse = "Version response: " + dependencies.versionTest.versionLib.versionValue();
-    var dependencyResponse = "Dependency response: " + dependencies.nesteddep2.foo.nested2()
+    var versionResponse = "Version response: " + dependencies.versionTest.versionLib().versionValue();
+    var dependencyResponse = "Dependency response: " + dependencies.nesteddep2.foo().nested2()
     return "{{From package [nesteddep1] |\n " + dependencyResponse + " |\n " + versionResponse + "}}";
 }
