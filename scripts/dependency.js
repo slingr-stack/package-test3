6d16eb27ff29002d8b53f5d31f1cca03cef0f679
Franco Ojeda <fojeda@slingr.io>
2023-05-22 16:20:31 -0300

    change function call

--- scripts/dependency.js
+++ scripts/dependency.js
@@ -1,13 +1,13 @@
 exports.getDepMsg = function(){
-    var response = dependencies.nesteddep1.foo2.getMessage();
+    var response = dependencies.nesteddep2.foo2.getMessage();
     return response;
 }
 
 exports.setDepMsg = function(value){
-    dependencies.nesteddep1.config.set('message', value);
+    dependencies.nesteddep2.config.set('message', value);
     return "Message configured on dependency with value:" + value
 }
 
 exports.callDepService = function () {
-    dependencies.nesteddep1.foo2.serviceTest()
+    dependencies.nesteddep2.foo2.serviceTest()
 }
